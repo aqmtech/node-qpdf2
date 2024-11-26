09236e93c8048833c574e0b9f57b320fac6beb0c
hinfai0 <112539219+hinfai0@users.noreply.github.com>
2024-11-26 13:06:18 +0800

    Changes to be committed: 	modified:   .babelrc 	new file:   dist/decrypt.d.ts 	new file:   dist/decrypt.d.ts.map 	renamed:    dist/cjs/decrypt.js -> dist/decrypt.js 	new file:   dist/encrypt.d.ts 	new file:   dist/encrypt.d.ts.map 	renamed:    dist/cjs/encrypt.js -> dist/encrypt.js 	deleted:    dist/esm/decrypt.js 	deleted:    dist/esm/encrypt.js 	deleted:    dist/esm/index.js 	deleted:    dist/esm/info.js 	deleted:    dist/esm/spawn.js 	deleted:    dist/esm/utils.js 	new file:   dist/index.d.ts 	new file:   dist/index.d.ts.map 	renamed:    dist/cjs/index.js -> dist/index.js 	new file:   dist/info.d.ts 	new file:   dist/info.d.ts.map 	renamed:    dist/cjs/info.js -> dist/info.js 	new file:   dist/spawn.d.ts 	new file:   dist/spawn.d.ts.map 	renamed:    dist/cjs/spawn.js -> dist/spawn.js 	new file:   dist/utils.d.ts 	new file:   dist/utils.d.ts.map 	renamed:    dist/cjs/utils.js -> dist/utils.js 	modified:   package.json 	modified:   tsconfig.build.json 	modified:   tsconfig.json

---
+++ dist/decrypt.d.ts
@@ -0,0 +1,15 @@
+export interface DecryptSettings {
+    /** The path for the encrypted pdf */
+    input: string;
+    /** The path for the decrypted pdf */
+    output?: string;
+    /** The password required for decrypting the pdf */
+    password?: string;
+}
+/**
+ * Decrypts a PDF
+ * @param payload The settings for decryption
+ * @returns The output of QPDF
+ */
+export declare const decrypt: (payload: DecryptSettings) => Promise<Buffer>;
+//# sourceMappingURL=decrypt.d.ts.map
